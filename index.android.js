a7cecb2a52ac834ac3246aa4f28eb073a9d929a2
Dean Harber <dean@codeswop.com>
2017-09-09 17:09:48 +0200

    Added a reset method

--- index.android.js
+++ index.android.js
@@ -3,5 +3,6 @@ import React from 'react-native';
 const NfcNdefReactNative = React.NativeModules.NfcNdefReactNative;
 
 export const readTag = NfcNdefReactNative.readTag;
+export const reset = NfcNdefReactNative.reset;
 export const writeTag = NfcNdefReactNative.writeTag;
 export const getTagId = NfcNdefReactNative.getTagId;
